0958fb711df862273c37e53b3ac8a43fb379ca00
RobertoHenryck <robertosilva.sesisenai@gmail.com>
2025-09-03 08:52:57 -0300

    Adicionando as imagens

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,22 +6,26 @@ if (!isset($_SESSION['auth'])) {
 
 <!doctype html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?= asset('css/home.css'); ?>">
     <script src="https://kit.fontawesome.com/904bf533d7.js" crossorigin="anonymous"></script>
+    <img src="../../../public/images/nexus erp logo.png" alt="Descrição da logo">
     <title>Página inicial</title>
 </head>
+
 <body>
 
-<?php view('components/header'); ?>
+    <?php view('components/header'); ?>
 
 
 
-<?php view('components/footer'); ?>
+    <?php view('components/footer'); ?>
 
 </body>
+
 </html>
