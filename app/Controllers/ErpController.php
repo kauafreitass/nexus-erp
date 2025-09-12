47800b9231f152455d59160f81f16bf62a4aaa50
Kauã Freitas <kauapessoadefreitas@gmail.com>
2025-09-12 07:43:21 -0300

    Atualizado outros módulos dos gráficos

--- app/Controllers/ErpController.php
+++ app/Controllers/ErpController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controllers;
 
+use App\Models\ErpModel;
+
 class ErpController
 {
 
-    public function index()
+    private ErpModel $model;
+
+    public function __construct()
+    {
+        $this->model = new ErpModel();
+    }
+
+    public function index(): void
     {
         view('erp/index');
     }
 
-    public function home(){
+    public function home(): void
+    {
         view('home');
+    }
+
+    public function salesChart($user)
+    {
+        $this->model->salesChart($user);
     }
 
 }
--- app/Models/ErpModel.php
+++ app/Models/ErpModel.php
@@ -2,7 +2,33 @@
 
 namespace App\Models;
 
+use Database\Database;
+
 class ErpModel
 {
 
+    private $pdo;
+
+    public function __construct() {
+        $database = new Database();
+        $this->pdo = $database->getConnection();
+    }
+
+    public function salesChart($user)
+    {
+        $sql = "SELECT * FROM sales_orders WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id', $user["id"]);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    public function customersChart($user)
+    {
+        $sql = "SELECT * FROM customers WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id', $user["id"]);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
 }
