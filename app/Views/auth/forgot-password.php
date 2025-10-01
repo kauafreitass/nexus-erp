b825146797b6955edcb0abd7538e8ba4f755726e
RobertoHenryck <robertosilva.sesisenai@gmail.com>
2025-10-01 08:30:38 -0300

    Finalizaçao dos css's das formas de login (login, cadastrar e esqueci senha)

--- app/Views/auth/forgot-password.php
+++ app/Views/auth/forgot-password.php
@@ -13,6 +13,33 @@
     <title>Recupere sua senha</title>
 </head>
 <body>
+  <div class="logo">
+    <img src="images/nexus-logo.png" alt="Logo">
+  </div>
+  <div class="container">
 
+    <div class="esquerda">
+
+      <h1>Esqueceu sua senha</h1>
+      <p>
+        Não se preocupe! Vamos te ajudar a recuperar seu acesso
+      </p>
+      <p>Para recuperar sua senha, confirme sua identidade respondendo à pergunta de segurança cadastrada. Após a validação, sua senha será atualizada!</p>
+    </div>
+    <div class="direita">
+      <form method="POST">
+        <h1>Esqueci minha senha</h1>
+
+        <input type="text" placeholder="E-mail">
+        <input type="password" placeholder="Senha">
+        <p class="esqueci-senha"><a href="forgot-password">Esqueci minha senha</a></p>
+
+        <button type="submit">Redefinir senha</button>
+      </form>
+
+    </div>
+
+
+  </div>
 </body>
 </html>
