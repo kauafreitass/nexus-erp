fd28f73363ac9235edfa501fe6a6785a7f7cf984
RobertoHenryck <robertosilva.sesisenai@gmail.com>
2025-10-01 07:28:11 -0300

    Començando á estilizar forgot_password

--- app/Views/auth/forgot-password.php
+++ app/Views/auth/forgot-password.php
@@ -9,8 +9,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= asset('css/auth.css'); ?>">
-    <title>Entre com sua conta</title>
+      <link rel="icon" href="images/nexus-logo.png" type="image/png">
+    <title>Recupere sua senha</title>
 </head>
 <body>
 
